3210db407523ec54677a58a46c992fb988beca82
Bukhtiychuk Vladimir <vpbukhti@mts.ru>
2025-03-16 22:30:07 +0300

    fix build

--- run_memepolice.sh
+++ run_memepolice.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
+cleanup() {
+    echo "{\"msg\": \"wrapper process killed\"}" >> "/var/log/memepolice.log"
+    pkill -P $$
+    exit 0
+}
+trap cleanup SIGINT SIGTERM
+
 source build_fpcalc.sh
 export BOT_TOKEN=$(cat secret.txt)
 go build -o memepolice .
+
 while true; do
     ./memepolice &>> "/var/log/memepolice.log"
     export EXIT_CODE=$?
     echo "{\"exit_code\": $EXIT_CODE}" >> "/var/log/memepolice.log"
+    sleep 1
 done
