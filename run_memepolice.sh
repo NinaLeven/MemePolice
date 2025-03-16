31fd1b3ef6b3d840b78d3416fc9e35258e5c0fd4
Bukhtiychuk Vladimir <vpbukhti@mts.ru>
2025-03-16 22:06:04 +0300

    fix run

--- run_memepolice.sh
+++ run_memepolice.sh
@@ -3,6 +3,8 @@ set -e
 source build_fpcalc.sh
 export BOT_TOKEN=$(cat secret.txt)
 go build -o memepolice .
-./memepolice &>> "/var/log/memepolice.log"
-export EXIT_CODE=$?
-echo "{\"exit_code\": $EXIT_CODE}" >> "/var/log/memepolice.log"
+while true; do
+    ./memepolice &>> "/var/log/memepolice.log"
+    export EXIT_CODE=$?
+    echo "{\"exit_code\": $EXIT_CODE}" >> "/var/log/memepolice.log"
+done
